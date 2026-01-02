ec3cba60edca2ef1689d51a4bc70ae6ec83e9c4c
unknown <mohdsshadab552004@gmail.com>
2026-01-02 17:07:42 +0530

    fix bug

--- frontend/src/api/admin/admin.api.ts
+++ frontend/src/api/admin/admin.api.ts
@@ -3,3 +3,4 @@ import api from "../axios";
 export const getAdminStats = () => api.get("/admin/stats");
 export const getAllHRs = () => api.get("/admin/hrs");
 export const getAllPlatforms = () => api.get("/platforms");
+export const getHRDetail = (id) => api.get(`/admin/hr/${id}`)
